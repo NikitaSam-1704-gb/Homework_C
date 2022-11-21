802e952857a3032eb106cd037eb36503e8c3d316
Никита <samnikita141204@yandex.ru>
2022-11-21 19:38:17 +0400

    Вывод результата сравнения двух чисел

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
-﻿int number = Convert.ToInt32(Console.ReadLine());
+﻿// Программа 1, вывод степени числа введеного с консоли
+//int number = Convert.ToInt32(Console.ReadLine());
+//int sq = (int)Math.Pow(number, 3) ;
+//Console.WriteLine(sq) ;
 
-int sq = (int)Math.Pow(number, 3) ;
 
-Console.WriteLine(sq) ;
+// Программа 2 Вывод результата сравнения двух чисел 
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1==(int)Math.Pow(number2, 2)) 
+  {
+   Console.WriteLine(" да ") ;
+  } 
+   else
+   {
+    Console.WriteLine(" нет ") ;
+   }
+  
