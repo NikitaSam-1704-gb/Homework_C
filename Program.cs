59befdd7df49bcf9522dbc66440e616690d17745
Никита <samnikita141204@yandex.ru>
2022-11-22 20:29:20 +0400

    Задача 8 д/з закомител текст

--- Program.cs
+++ Program.cs
@@ -54,16 +54,15 @@
 
 
 //Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
- Console.WriteLine("Введите число, количество элементов натурального множества") ;
- int num = Convert.ToInt32(Console.ReadLine()) ;
- int i=1 ;
- Console.Write(" четные числа данного множества") ;
-while ( i<=num ) {
-
-  if (i%2==0)
-  {
-   Console.Write($" {i} ") ;
-  }
-  i++ ;
- }
- Console.WriteLine() ;
+ //Console.WriteLine("Введите число, количество элементов натурального множества") ;
+ //int num = Convert.ToInt32(Console.ReadLine()) ;
+ //int i=1 ;
+ //Console.Write(" четные числа данного множества") ;
+//while ( i<=num ) {
+//  if (i%2==0)
+//  {
+//   Console.Write($" {i} ") ;
+//  }
+//  i++ ;
+// }
+// Console.WriteLine() ;
