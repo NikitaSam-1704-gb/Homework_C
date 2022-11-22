9368df7da92cb3764cd6cbeafcdacd1176b32079
Никита <samnikita141204@yandex.ru>
2022-11-22 17:08:48 +0400

    сранения числа и квадрата

--- Program.cs
+++ Program.cs
@@ -4,15 +4,15 @@
 //Console.WriteLine(sq) ;
 
 
-// Программа 2 Вывод результата сравнения двух чисел 
-int number1 = Convert.ToInt32(Console.ReadLine());
-int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1==(int)Math.Pow(number2, 2)) 
-  {
-   Console.WriteLine(" да ") ;
-  } 
-   else
-   {
-    Console.WriteLine(" нет ") ;
-   }
+// Программа 2 Вывод результата сравнения двух чисел, если первое равно квадрату второго, выводи "Да", если не равно  то "Нет"
+//int number1 = Convert.ToInt32(Console.ReadLine());
+//int number2 = Convert.ToInt32(Console.ReadLine());
+//if (number1==(int)Math.Pow(number2, 2)) 
+//  {
+//   Console.WriteLine(" да ") ;
+//  } 
+//   else
+//   {
+//    Console.WriteLine(" нет ") ;
+//   }
   
