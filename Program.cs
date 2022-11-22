08a761b041c2beee951ca55af538f8a26dff7fd4
Никита <samnikita141204@yandex.ru>
2022-11-22 18:03:02 +0400

    Задача 4 дз

--- Program.cs
+++ Program.cs
@@ -16,16 +16,35 @@
 //    Console.WriteLine(" нет ") ;
 //   }
   
+
 // Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
-Console.Write("Введите первое число ");
-int num1 =Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите первое число ");
-int num2=Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите первое число ");
+//int num1 =Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите первое число ");
+//int num2=Convert.ToInt32(Console.ReadLine());
+//if (num1>num2)
+//  Console.WriteLine($" max {num1}");
+//else 
+//  Console.WriteLine($" max {num2}");
+
+
+//  Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите три числа  ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1 ;
 if (num1>num2)
-  Console.WriteLine($" max {num1}");
-else 
-  Console.WriteLine($" max {num2}");
+  max = num1; 
+else
+   max = num2 ;
+
+
+if (num3>max)
+  max=num3 ;
+ Console.WriteLine($"max> {max}") ;
 
